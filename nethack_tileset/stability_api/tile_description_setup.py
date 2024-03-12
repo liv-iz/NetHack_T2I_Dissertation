d75bdfeabb5140c2e69f966ecb0d3571521d68b6
Olivia Issler <14isslero@gmail.com>
2024-03-12 11:18:39 +0000

    added multi-prompting

--- nethack_tileset/stability_api/tile_description_setup.py
+++ nethack_tileset/stability_api/tile_description_setup.py
@@ -6,6 +6,6 @@ tile_names = pd.read_csv("tileset_vanilla_titles.csv")
 tile_descriptions = pd.read_csv("tileset_gpt_4_answers.csv")
 
 tile_names = tile_names.join(tile_descriptions)
-tile_names['prompt'] = tile_names["Title"] + ' from the game Nethack. ' + (tile_names["GPT-4 description"] + 'In the style of Van Gogh.')
-
+tile_names['prompt'] = tile_names["Title"] + ' from the game Nethack. ' + (tile_names["GPT-4 description"] + 'In the style of Pixel Art.')
+tile_names['neg_prompt'] = '2D, shadows, dark, expansive, 8K, 4K'
 tile_names[:10].to_csv("tileset_prompts_with_tile_name.csv", index=False)
